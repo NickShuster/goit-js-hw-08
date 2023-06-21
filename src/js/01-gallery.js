3583b28f3b7bbdc5e7efc26673de319364ec6536
NickShuster <124299055+NickShuster@users.noreply.github.com>
2023-06-21 15:11:58 +0200

    submit and gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,52 +1,23 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items.js';
 
-const gallery = document.querySelector('.gallery');
+const createGalleryItem = ({ preview, original, description }) => `
+  <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+  </li>
+`;
 
-const createGalleryItem = ({ preview, original, description }) => {
-  return `
-    <li class="gallery__item">
-      <a class="gallery__link" href="${original}">
-        <img
-          class="gallery__image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
-        />
-      </a>
-    </li>
-  `;
-};
+const createGalleryItemsMarkup = items => items.map(createGalleryItem).join('');
 
-const renderGallery = (galleryItems) => {
-  const galleryMarkup = galleryItems
-    .map((item) => createGalleryItem(item))
-    .join('');
+const galleryContainer = document.querySelector('.gallery');
+galleryContainer.innerHTML = createGalleryItemsMarkup(galleryItems);
 
-  gallery.innerHTML = galleryMarkup;
-};
-
-renderGallery(galleryItems);
-
-gallery.addEventListener('click', (event) => {
-  event.preventDefault();
-
-  if (event.target.classList.contains('gallery__image')) {
-    const largeImageUrl = event.target.dataset.source;
-
-    const lightbox = new SimpleLightbox('.gallery a', {
-      captionsData: 'alt',
-      captionDelay: 250,
-      captionPosition: 'bottom',
-      overlay: true,
-      animationSpeed: 250,
-      fadeSpeed: 250,
-      closeText: '×',
-      swipeClose: true,
-      history: true,
-    });
-
-    lightbox.open({ startAt: galleryItems.findIndex(item => item.original === largeImageUrl) });
-  }
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
 });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,52 +1,10 @@
-// import throttle from 'lodash.throttle';
-
-// const feedbackForm = document.querySelector('.feedback-form');
-// const emailInput = feedbackForm.querySelector('input[name="email"]');
-// const messageInput = feedbackForm.querySelector('textarea[name="message"]');
-
-// const feedbackFormStateKey = 'feedback-form-state';
-// const saveFormStateToLocalStorage = () => {
-//   const formState = {
-//     email: emailInput.value,
-//     message: messageInput.value
-//   };
-//   localStorage.setItem(feedbackFormStateKey, JSON.stringify(formState));
-// };
-// const fillFormFromLocalStorage = () => {
-//   const storedState = localStorage.getItem(feedbackFormStateKey);
-//   if (storedState) {
-//     const formState = JSON.parse(storedState);
-//     emailInput.value = formState.email;
-//     messageInput.value = formState.message;
-//   }
-// };
-
-// const clearLocalStorageAndForm = () => {
-//   localStorage.removeItem(feedbackFormStateKey);
-//   emailInput.value = '';
-//   messageInput.value = '';
-// };
-
-// const handleSubmit = (event) => {
-//   event.preventDefault();
-//   const formState = {
-//     email: emailInput.value,
-//     message: messageInput.value
-//   };
-//   console.log(formState);
-//   clearLocalStorageAndForm();
-// };
-
-// feedbackForm.addEventListener('input', throttle(saveFormStateToLocalStorage, 500));
-
-// fillFormFromLocalStorage();
-// feedbackForm.addEventListener('submit', handleSubmit);
 
 import throttle from 'lodash.throttle';
 
 const feedbackForm = document.querySelector('.feedback-form');
 const emailInput = feedbackForm.querySelector('input[name="email"]');
 const messageInput = feedbackForm.querySelector('textarea[name="message"]');
+const submitButton = feedbackForm.querySelector('button[type="submit"]');
 
 const feedbackFormStateKey = 'feedback-form-state';
 const saveFormStateToLocalStorage = () => {
@@ -74,11 +32,19 @@ const clearLocalStorageAndForm = () => {
 
 const handleSubmit = (event) => {
   event.preventDefault();
-  const formState = {
-    email: emailInput.value,
-    message: messageInput.value
-  };
-  console.log(formState);
+
+  const isEmailEmpty = emailInput.value.trim() === '';
+  const isMessageEmpty = messageInput.value.trim() === '';
+
+  if (isEmailEmpty || isMessageEmpty) {
+    showAlert();
+  } else {
+    const formState = {
+      email: emailInput.value,
+      message: messageInput.value
+    };
+    console.log(formState);
+  }
 };
 
 const showAlert = () => {
@@ -89,7 +55,4 @@ feedbackForm.addEventListener('input', throttle(saveFormStateToLocalStorage, 500
 
 fillFormFromLocalStorage();
 
-emailInput.addEventListener('input', showAlert);
-messageInput.addEventListener('input', showAlert);
-
-feedbackForm.addEventListener('submit', handleSubmit);
+submitButton.addEventListener('click', handleSubmit);
